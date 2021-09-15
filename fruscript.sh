1e88b1d2a59641d8be9389c6dc976222929237ff
ltechlee <js1127@ltechkorea.com>
2021-09-15 17:54:12 +0900

    first step

--- fruscript.sh
+++ fruscript.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "hello, world"
-printf "hello, world\n"
-
-#second edit
+for((;;))
+do
+    # Insert IP
+    echo "Insert BMC IP Address."
+    read IP
+    # IP string format validation
+    if [[  $IP =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
+        echo "BMC IP Address : $IP"
+        break
+    else
+        echo "'$IP' is not ip address. please try again."
+    fi
+done
