0db859d904b643b15e717ded6ec62f7fe6007444
ltechlee <js1127@ltechkorea.com>
2021-09-27 15:46:01 +0900

    Version 1.0.0

--- fruscript.sh
+++ fruscript.sh
@@ -1,9 +1,150 @@
 #!/bin/bash
 
+# This code was inspired by the open source C code of the APT progress bar
+# http://bazaar.launchpad.net/~ubuntu-branches/ubuntu/trusty/apt/trusty/view/head:/apt-pkg/install-progress.cc#L233
+
+#
+# Usage:
+# Source this script
+# setup_scroll_area
+# draw_progress_bar 10
+# draw_progress_bar 90
+# destroy_scroll_area
+#
+CODE_SAVE_CURSOR="\033[s"
+CODE_RESTORE_CURSOR="\033[u"
+CODE_CURSOR_IN_SCROLL_AREA="\033[1A"
+COLOR_FG="\e[30m"
+COLOR_BG="\e[42m"
+RESTORE_FG="\e[39m"
+RESTORE_BG="\e[49m"
+
+function setup_scroll_area() {
+    lines=$(tput lines)
+    let lines=$lines-1
+    # Scroll down a bit to avoid visual glitch when the screen area shrinks by one row
+    echo -en "\n"
+
+    # Save cursor
+    echo -en "$CODE_SAVE_CURSOR"
+    # Set scroll region (this will place the cursor in the top left)
+    echo -en "\033[0;${lines}r"
+
+    # Restore cursor but ensure its inside the scrolling area
+    echo -en "$CODE_RESTORE_CURSOR"
+    echo -en "$CODE_CURSOR_IN_SCROLL_AREA"
+
+    # Start empty progress bar
+    draw_progress_bar 0
+}
+
+function destroy_scroll_area() {
+    lines=$(tput lines)
+    # Save cursor
+    echo -en "$CODE_SAVE_CURSOR"
+    # Set scroll region (this will place the cursor in the top left)
+    echo -en "\033[0;${lines}r"
+
+    # Restore cursor but ensure its inside the scrolling area
+    echo -en "$CODE_RESTORE_CURSOR"
+    echo -en "$CODE_CURSOR_IN_SCROLL_AREA"
+
+    # We are done so clear the scroll bar
+    clear_progress_bar
+
+    # Scroll down a bit to avoid visual glitch when the screen area grows by one row
+    echo -en "\n\n"
+}
+
+function draw_progress_bar() {
+    percentage=$1
+    lines=$(tput lines)
+    let lines=$lines
+    # Save cursor
+    echo -en "$CODE_SAVE_CURSOR"
+
+    # Move cursor position to last row
+    echo -en "\033[${lines};0f"
+
+    # Clear progress bar
+    tput el
+
+    # Draw progress bar
+    print_bar_text $percentage
+
+    # Restore cursor position
+    echo -en "$CODE_RESTORE_CURSOR"
+}
+
+function clear_progress_bar() {
+    lines=$(tput lines)
+    let lines=$lines
+    # Save cursor
+    echo -en "$CODE_SAVE_CURSOR"
+
+    # Move cursor position to last row
+    echo -en "\033[${lines};0f"
+
+    # clear progress bar
+    tput el
+
+    # Restore cursor position
+    echo -en "$CODE_RESTORE_CURSOR"
+}
+
+function print_bar_text() {
+    local percentage=$1
+
+    # Prepare progress bar
+    let remainder=100-$percentage
+    progress_bar=$(echo -ne "$percentage%" "["; echo -en "${COLOR_FG}${COLOR_BG}"; printf_new "#" $percentage; echo -en "${RESTORE_FG}${RESTORE_BG}"; printf_new "." $remainder; echo -ne "]");
+
+    # Print progress bar
+    if [ $1 -gt 99 ]
+    then
+        echo -ne "${progress_bar}"
+    else
+        echo -ne "${progress_bar}"
+    fi
+}
+
+printf_new() {
+    str=$1
+    num=$2
+    v=$(printf "%-${num}s" "$str")
+    echo -ne "${v// /$str}"
+}
+
+# ------------------------------------------------------------------------------------------
+NowDate=`date`
+log_file="Change_history_log.txt"
+version=" LTechKorea FRU Editor v1.0.0 (2021. 09. 27.)"
+
+echo -n '
+
+██╗  ████████╗███████╗ ██████╗██╗  ██╗██╗  ██╗ ██████╗ ██████╗ ███████╗ █████╗        ██╗███╗   ██╗ ██████╗        
+██║  ╚══██╔══╝██╔════╝██╔════╝██║  ██║██║ ██╔╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗       ██║████╗  ██║██╔════╝        
+██║     ██║   █████╗  ██║     ███████║█████╔╝ ██║   ██║██████╔╝█████╗  ███████║       ██║██╔██╗ ██║██║             
+██║     ██║   ██╔══╝  ██║     ██╔══██║██╔═██╗ ██║   ██║██╔══██╗██╔══╝  ██╔══██║       ██║██║╚██╗██║██║             
+███████╗██║   ███████╗╚██████╗██║  ██║██║  ██╗╚██████╔╝██║  ██║███████╗██║  ██║▄█╗    ██║██║ ╚████║╚██████╗        
+╚══════╝╚═╝   ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝    ╚═╝╚═╝  ╚═══╝ ╚═════╝        
+                                                                                                                   
+███████╗██████╗ ██╗   ██╗    ███████╗██████╗ ██╗████████╗ ██████╗ ██████╗     ██╗   ██╗ ██╗    ██████╗     ██████╗ 
+██╔════╝██╔══██╗██║   ██║    ██╔════╝██╔══██╗██║╚══██╔══╝██╔═══██╗██╔══██╗    ██║   ██║███║   ██╔═████╗   ██╔═████╗
+█████╗  ██████╔╝██║   ██║    █████╗  ██║  ██║██║   ██║   ██║   ██║██████╔╝    ██║   ██║╚██║   ██║██╔██║   ██║██╔██║
+██╔══╝  ██╔══██╗██║   ██║    ██╔══╝  ██║  ██║██║   ██║   ██║   ██║██╔══██╗    ╚██╗ ██╔╝ ██║   ████╔╝██║   ████╔╝██║
+██║     ██║  ██║╚██████╔╝    ███████╗██████╔╝██║   ██║   ╚██████╔╝██║  ██║     ╚████╔╝  ██║██╗╚██████╔╝██╗╚██████╔╝
+╚═╝     ╚═╝  ╚═╝ ╚═════╝     ╚══════╝╚═════╝ ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝      ╚═══╝   ╚═╝╚═╝ ╚═════╝ ╚═╝ ╚═════╝ 
+                                                                                                                   
+'
+# http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=LTechKorea%2C%20Inc%0AFRU%20Editor%20v1.0.0
+echo "$version"
+
+
 for((;;))
 do
     # IP입력
-    echo -n -e "-------------------------------\nInsert BMC IP Address : "
+    echo -n -e "--------------------------------------------\nInsert BMC IP Address : "
     read IP
     if [ ${IP,,} = 'exit' ]; then
         break
@@ -24,12 +165,23 @@ stty -echo
 read password
 stty echo
 echo -e \n
-echo '-------------------------------'
-
-ipmitool -H $IP -U $username -P $password fru
-if [ $? != 0 ]; then # 서버 연결 접속안될때 나가기
+echo '--------------------------------------------'
+# 서버 연결 접속안될때 나가기
+ipmitool -H $IP -U $username -P $password fru >>/dev/null 2>&1
+if [ $? != 0 ]; then
 exit
 fi
+echo "================Before FRU Information================"
+ipmitool -H $IP -U $username -P $password fru
+setup_scroll_area
+
+# 변경 전 로그 저장
+echo "-------------$NowDate-------------" >> $log_file
+echo "$version" >> $log_file
+echo -e "\nConnection information.\n IP : $IP\n UserName : $username\n Password : $password\n" >> $log_file
+echo "================Before FRU Information================" >> $log_file
+ipmitool -H $IP -U $username -P $password fru >> $log_file
+
 # FRU ID 0 정보 가져오기
 fru0=$(ipmitool -H $IP -U $username -P $password fru print 0)
 CT0=$(echo "$fru0" | awk -F ':' '{if ($1~"Chassis Type") { print $2 }}' | tr -d ,'\12')
@@ -47,7 +199,9 @@ PPN0=$(echo "$fru0" | awk -F ':' '{if ($1~"Product Part Number") { print $2 }}' 
 PV0=$(echo "$fru0" | awk -F ':' '{if ($1~"Product Version") { print $2 }}' | tr -d ' ','\12')
 PS0=$(echo "$fru0" | awk -F ':' '{if ($1~"Product Serial") { print $2 }}' | tr -d ' ','\12')
 PAT0=$(echo "$fru0" | awk -F ':' '{if ($1~"Product Asset Tag") { print $2 }}' | tr -d ' ','\12')
+
 # FRU ID 1 정보 가져오기
+draw_progress_bar 10
 fru1=$(ipmitool -H $IP -U $username -P $password fru print 1)
 CT1=$(echo "$fru1" | awk -F ':' '{if ($1~"Chassis Type") { print $2 }}' | tr -d ,'\12')
 CPN1=$(echo "$fru1" | awk -F ':' '{if ($1~"Chassis Part Number") { print $2 }}' | tr -d ' ','\12')
@@ -64,6 +218,7 @@ PPN1=$(echo "$fru1" | awk -F ':' '{if ($1~"Product Part Number") { print $2 }}' 
 PV1=$(echo "$fru1" | awk -F ':' '{if ($1~"Product Version") { print $2 }}' | tr -d ' ','\12')
 PS1=$(echo "$fru1" | awk -F ':' '{if ($1~"Product Serial") { print $2 }}' | tr -d ' ','\12')
 PAT1=$(echo "$fru1" | awk -F ':' '{if ($1~"Product Asset Tag") { print $2 }}' | tr -d ' ','\12')
+draw_progress_bar 20
 
 # Chassis Part Number
 ipmitool -H $IP -U $username -P $password fru edit 0 field c 0 "LKG000000000000$(echo $PN1 | awk -F '-' '{print $2}')" >>/dev/null 2>&1
@@ -73,12 +228,15 @@ ipmitool -H $IP -U $username -P $password fru edit 0 field c 1 "$PS" >>/dev/null
 ipmitool -H $IP -U $username -P $password fru edit 0 field b 0 "LTechKorea, Inc." >>/dev/null 2>&1
 # Board Product
 ipmitool -H $IP -U $username -P $password fru edit 0 field b 1 "B-LKG-$BP1" >>/dev/null 2>&1
+draw_progress_bar 40
 # Board Part Number
 ipmitool -H $IP -U $username -P $password fru edit 0 field b 3 "B000000000000$(echo $PN1 | awk -F '-' '{print $2}')" >>/dev/null 2>&1
 # Board Extra
 ipmitool -H $IP -U $username -P $password fru edit 0 field b 5 " " >>/dev/null 2>&1
 # Product Manufacturer
 ipmitool -H $IP -U $username -P $password fru edit 0 field p 0 "LTechKorea, Inc." >>/dev/null 2>&1
+draw_progress_bar 60
+
 # Product Name
 case $(echo $PN1 | awk -F '-' '{print $2}') in
     3C0)
@@ -100,18 +258,23 @@ case $(echo $PN1 | awk -F '-' '{print $2}') in
         ipmitool -H $IP -U $username -P $password fru edit 0 field p 1 "LKG-1210-C" >>/dev/null 2>&1
         ;;       
 esac
+draw_progress_bar 80
 # Product Part Number
 ipmitool -H $IP -U $username -P $password fru edit 0 field p 2 "LKG000000000000$(echo $PN1 | awk -F '-' '{print $2}')" >>/dev/null 2>&1
 # Product Asset Tag
 ipmitool -H $IP -U $username -P $password fru edit 0 field p 5 " " >>/dev/null 2>&1
-
 # FRU ID 1, Board Mfg
-ipmitool -H $IP -U $username -P $password fru edit 1 field p 5 "LTechKorea, Inc." >>/dev/null 2>&1
+ipmitool -H $IP -U $username -P $password fru edit 1 field b 0 "LTechKorea, Inc." >>/dev/null 2>&1
+draw_progress_bar 90
 # FRU ID 1, Board Serial
-ipmitool -H $IP -U $username -P $password fru edit 1 field p 5 "$BS0" >>/dev/null 2>&1
+ipmitool -H $IP -U $username -P $password fru edit 1 field b 2 "$BS0" >>/dev/null 2>&1
 # FRU ID 1, Product Serial
-ipmitool -H $IP -U $username -P $password fru edit 1 field p 5 "$PS0" >>/dev/null 2>&1
-
-echo '-------------------------------'
-# 확인 출력
-ipmitool -H $IP -U $username -P $password fru 
+ipmitool -H $IP -U $username -P $password fru edit 1 field p 4 "$PS0" >>/dev/null 2>&1
+destroy_scroll_area
+# 확인 출력 및 로그 저장
+echo "================After FRU Information================"
+echo "================After FRU Information================">> $log_file
+ipmitool -H $IP -U $username -P $password fru
+ipmitool -H $IP -U $username -P $password fru >> $log_file
+echo "Complete."
+echo "Save the log. [`pwd`/$log_file]"
